f0ae079b2bf1edfb66683355c938876898d1c29f
wfsovereign <wfsovereign@outlook.com>
2016-11-15 23:46:09 +0800

    add order controller demo file

---
+++ controller/order.js
@@ -0,0 +1,14 @@
+exports.list = function *() {
+  return this.body = [
+    {
+      id: '1',
+      username: 'wfsovereign',
+      title: 'hello'
+    },
+    {
+      id: '2',
+      username: 'node',
+      title: 'world'
+    }
+  ];
+};
